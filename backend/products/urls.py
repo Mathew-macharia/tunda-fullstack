012d59a543d4585a7786065580ae5d714c1ba952
Mathew Kamande Macharia <mathew.macharia@ashesi.edu.gh>
2025-08-12 20:39:11 +0300

    updated products/urls.py defaultrouter

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductCategoryViewSet, ProductViewSet, ProductListingViewSet
 
-router = DefaultRouter
+router = DefaultRouter()
 router.register(r'categories', ProductCategoryViewSet)
 router.register(r'listings', ProductListingViewSet, basename='product-listing')
 router.register(r'items', ProductViewSet, basename='product')
